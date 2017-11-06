5b9de42885ac653bf146b3eedb54e3c0f75cccc3
Raphael Michel <mail@raphaelmichel.de>
2017-11-06 10:00:00 +0100

    Really use a submodule

--- pretixdroid/libpretixsync-repo/libpretixsync/src/main/java/eu/pretix/libpretixsync/api/HttpClientFactory.java
+++
@@ -1,7 +0,0 @@
-package eu.pretix.libpretixsync.api;
-
-import okhttp3.OkHttpClient;
-
-public interface HttpClientFactory {
-    public OkHttpClient buildClient();
-}
--- pretixdroid/libpretixsync-repo/libpretixsync/src/main/java/eu/pretix/libpretixsync/check/CheckException.java
+++
@@ -1,8 +0,0 @@
-package eu.pretix.libpretixsync.check;
-
-
-public class CheckException extends Exception {
-    public CheckException(String msg) {
-        super(msg);
-    }
-}
